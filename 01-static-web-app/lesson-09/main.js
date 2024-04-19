18321d77df65a1a48476d464c8cec30c1fb22165
abductedRhino <avocadorable@mailbox.org>
2024-04-19 13:42:31 +0200

    use url and body encoding

--- 01-static-web-app/lesson-09/main.js
+++ 01-static-web-app/lesson-09/main.js
@@ -2,13 +2,6 @@ const port = 3000;
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
-  console.log(req.params);  // --> {}
-  console.log(req.body);  // --> undefined
-  console.log(req.url);  // --> /
-  console.log(req.query);  // --> {}
-  res.send('Hello, Universe!');
-})
 app.use((req, res, next) => {
   console.log(`incoming request to: ${req.url}`);
   next();
@@ -16,6 +9,25 @@ app.use((req, res, next) => {
 app.use('/items', (req, res, next) => {
   console.log('Special request');
   next();
+})
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+// curl --data "first_name=Jon&last_name=Doe" 127.0.0.1:3000
+// --> body :  [Object: null prototype] { name: 'Jon', othername: 'Doe' }
+// --> query:  {}
+
+app.post('/', (req, res) => {
+  console.log('body : ', req.body);
+  console.log('query: ', req.query);
+  res.send('Submitted.\n');
+});
+
+app.get('/', (req, res) => {
+  console.log(req.params);  // --> {}
+  console.log(req.body);  // --> undefined
+  console.log(req.url);  // --> /
+  console.log(req.query);  // --> {}
+  res.send('Hello, Universe!');
 })
 // curl --data '' 127.0.0.1:3000/contact
 app.post('/contact', (req, res) => {
