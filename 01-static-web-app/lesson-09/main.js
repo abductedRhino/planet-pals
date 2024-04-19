2691451a5fc7c194e4fed913e02d92625baad07b
abductedRhino <avocadorable@mailbox.org>
2024-04-19 13:26:47 +0200

    Add simple middleware function and one that is only for '/items'

--- 01-static-web-app/lesson-09/main.js
+++ 01-static-web-app/lesson-09/main.js
@@ -9,7 +9,14 @@ app.get('/', (req, res) => {
   console.log(req.query);  // --> {}
   res.send('Hello, Universe!');
 })
-
+app.use((req, res, next) => {
+  console.log(`incoming request to: ${req.url}`);
+  next();
+})
+app.use('/items', (req, res, next) => {
+  console.log('Special request');
+  next();
+})
 // curl --data '' 127.0.0.1:3000/contact
 app.post('/contact', (req, res) => {
   res.send('Submitted.\n');
