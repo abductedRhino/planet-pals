1a6d4af959208f6324de62be6d89e3826a967171
abductedRhino <avocadorable@mailbox.org>
2024-04-20 14:50:31 +0200

    Restructure to use MVC

--- 01-static-web-app/lesson-09/main.js
+++ 01-static-web-app/lesson-09/main.js
@@ -1,17 +1,11 @@
 const port = 3000;
 const express = require('express');
 const app = express();
+const homeController = require('./controllers/homeController');
 
 // http://127.0.0.1:3000/?cart=3&pagesVisited=4&utmcode=1234
-app.use((req, res, next) => {
-  console.log('middleware=app.use');
-  console.log(req.query)  // --> { cart: '3', pagesVisited: '4', utmcode: '1234' }
-  next();
-})
-app.use('/items', (req, res, next) => {
-  console.log('middleware=app.use /items');
-  next();
-})
+app.use(homeController.logRequestPath);
+app.use('/items', homeController.logRequestPathItems);
 // use middleware that analyses incoming request bodies that are url-encoded.
 // urlencoded are form posts and utf-8 content.
 app.use(express.urlencoded({ extended: false }));
@@ -19,27 +13,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // curl --data "first_name=Jon&last_name=Doe" 127.0.0.1:3000
-app.post('/', (req, res) => {
-  console.log('body : ', req.body);
-  console.log('query: ', req.query);
-  res.send('Submitted.\n');
-});
+app.post('/', homeController.postIndex);
 
-app.get('/', (req, res) => {
-  console.log(req.params);  // --> {}
-  console.log(req.body);  // --> undefined
-  console.log(req.url);  // --> /
-  console.log(req.query);  // --> {}
-  res.send('Hello, Universe!');
-})
+app.get('/', homeController.getIndex);
 // curl --data '' 127.0.0.1:3000/contact
-app.post('/contact', (req, res) => {
-  res.send('Submitted.\n');
-})
+app.post('/contact', homeController.postContact);
 // curl 127.0.0.1:3000/items/apple
-app.get('/items/:vegetable', (req, res) => {
-  res.send(`vegetable: ${req.params.vegetable}\n`);
-})
+app.get('/items/:vegetable', homeController.getItemsVegetable);
 app.listen(port, () => {
   console.log(`Listening on port ${port}.`);
 })
