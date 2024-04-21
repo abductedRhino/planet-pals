fbb67daa8616fc2642ca54c96d82b148b32f2989
abductedRhino <avocadorable@mailbox.org>
2024-04-21 15:20:57 +0200

    Init lesson 11

---
+++ 01-static-web-app/lesson-11/main.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+const homeController = require('./controllers/homeController');
+const errorController = require('./controllers/errorController');
+const layouts = require('express-ejs-layouts');
+
+app.set('port', process.env.PORT || 3000);
+app.set('view engine', 'ejs');
+// http://127.0.0.1:3000/?cart=3&pagesVisited=4&utmcode=1234
+app.use(homeController.logRequestPath);
+app.use(errorController.logErrors);
+app.use('/items', homeController.logRequestPathItems);
+app.use(layouts);
+// use middleware that analyses incoming request bodies that are url-encoded.
+// urlencoded are form posts and utf-8 content.
+app.use(express.urlencoded({ extended: false }));
+// use format json to parse middleware.
+app.use(express.json());
+
+// curl --data "first_name=Jon&last_name=Doe" 127.0.0.1:3000
+app.post('/', homeController.postIndex);
+
+app.get('/', homeController.getIndex);
+// curl --data '' 127.0.0.1:3000/contact
+app.post('/contact', homeController.postContact);
+// curl 127.0.0.1:3000/items/apple
+app.get('/items/:vegetable', homeController.getItemsVegetable);
+app.listen(app.get('port'), () => {
+  console.log(`Listening on port ${app.get('port')}.`);
+})
+app.get('/name/:name', homeController.respondWithName);
